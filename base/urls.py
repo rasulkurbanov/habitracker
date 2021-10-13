0dc1077fdeb61b8481c703613db3a0b82f5d0e1a
Rasuljon <rasuljohn139@gmail.com>
2021-10-13 16:06:22 +0500

    Added CRUD, search functionality

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import TaskCreate, TaskDelete, TaskUpdate, TasksList, TaskDetail
+from django.utils.regex_helper import next_char
+from .views import CustomLoginView, RegisterPage, TaskCreate, TaskDelete, TaskUpdate, TasksList, TaskDetail
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout', LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', RegisterPage.as_view(), name="register"),
     path('', TasksList.as_view(), name='tasks'),
     path('task-create', TaskCreate.as_view(), name='task-create'),
     path('task/<int:pk>', TaskDetail.as_view(), name='task-detail'),
--- base/views.py
+++ base/views.py
@@ -1,46 +1,88 @@
-from django.db import models
+from django.shortcuts import redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import UpdateView, DeleteView, CreateView
+from django.views.generic.edit import UpdateView, DeleteView, CreateView,FormView
+from django.contrib.auth.views import LoginView, redirect_to_login
 from django.urls import reverse_lazy
-
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from base.models import Task
 
 # Create your views here.
 
-#View that lists all tasks
-class TasksList(ListView):
+# View that lists all tasks
+
+
+class CustomLoginView(LoginView):
+    template_name = 'login.html'
+    fields = "__all__"
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
+
+
+class RegisterPage(FormView):
+    template_name = 'register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super(RegisterPage, self).get(*args, **kwargs)
+
+class TasksList(LoginRequiredMixin, ListView):
     model = Task
     template_name = 'task_list.html'
+    context_object_name = 'tasks'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count'] = context['tasks'].filter(complete=False).count()
+
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['tasks'] = context['tasks'].filter(title__startswith=search_input)
+
+        context['search_input'] = search_input
+        return context
 
 
-class TaskDetail(DetailView):
+class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     template_name = 'task_detail.html'
 
-
-class TaskCreate(CreateView):
+   
+class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
     template_name = 'task_create_form.html'
     success_url = reverse_lazy('tasks')
-    fields = ['title', 'description']
+    fields = ['title', 'description', 'complete']
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TaskCreate, self).form_valid(form)
 
 
-class TaskUpdate(UpdateView):
+class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
     fields = ['title', 'description', 'complete']
     template_name = 'task_form.html'
     success_url = reverse_lazy('tasks')
 
 
-
-class TaskDelete(DeleteView):
+class TaskDelete(LoginRequiredMixin, DeleteView):
     model = Task
     template_name = 'task_delete.html'
     success_url = reverse_lazy('tasks')
-
-
-
-
-
 
